7bbbc7294e28854dac7fd5e8f1b399aed64c172e
Lev Zakharov <l.j.zakharov@gmail.com>
2019-05-08 17:15:01 +0300

    Add solutions to the Sorting section

---
+++ Sorting/Comparator.py
@@ -0,0 +1,26 @@
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return f'{name} {score}'
+
+    def comparator(self, other):
+        return self.score - other.score
+
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
