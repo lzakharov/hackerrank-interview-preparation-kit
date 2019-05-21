85915e555995705bb86da247c8708417fcb65741
Lev Zakharov <l.j.zakharov@gmail.com>
2019-05-22 01:36:28 +0300

    Rewrite solution to the Friend Circle Queries problem

--- Miscellaneous/FriendCircleQueries.py
+++ Miscellaneous/FriendCircleQueries.py
@@ -1,40 +1,45 @@
-def root(parent, a):
-    while a != parent[a]:
-        a = parent[a]
-    return a
+N = 10 ** 9
+
+
+class DisjointSet:
+    def __init__(self):
+        self.parent = {}
+        self.size = {}
+
+    def join(self, x, y):
+        self.parent[x] = self.parent.get(x, x)
+        self.parent[y] = self.parent.get(y, y)
+        self.size[x] = self.size.get(x, 1)
+        self.size[y] = self.size.get(y, 1)
+        px = self.root(x)
+        py = self.root(y)
+
+        if px != py:
+            if self.size[px] > self.size[py]:
+                self.parent[py] = self.parent[px]
+                self.size[px] += self.size[py]
+            else:
+                self.parent[px] = self.parent[py]
+                self.size[py] += self.size[px]
+
+        return max(self.size[px], self.size[py])
+
+    def root(self, x):
+        while self.parent[x] != x:
+            x = self.parent[x]
+        return x
 
 
 def main():
-    queries = int(input())
+    qs = int(input())
+    ds = DisjointSet()
+    max_size = 0
 
-    parent = {}
-    size = {}
-    answer = [1] * queries
-    max_size = 1
-
-    for query in range(queries):
-        a, b = map(int, input().split())
-        parent[a] = parent.get(a, a)
-        parent[b] = parent.get(b, b)
-        size[a] = size.get(a, 1)
-        size[b] = size.get(b, 1)
-
-        pa = root(parent, a)
-        pb = root(parent, b)
-
-        if pa != pb:
-            if size[pa] > size[pb]:
-                parent[pb] = pa
-                size[pa] += size[pb]
-            else:
-                parent[pa] = pb
-                size[pb] += size[pa]
-
-        max_size = max(max_size, max(size[pa], size[pb]))
-
-        answer[query] = max_size
-
-    print(*answer, sep='\n')
+    for _ in range(qs):
+        x, y = map(int, input().split())
+        size = ds.join(x, y)
+        max_size = max(max_size, size)
+        print(max_size)
 
 
 if __name__ == '__main__':
